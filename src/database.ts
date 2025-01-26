8e46c756eb1cec84c8c781a5e8cb71bff9bfc1fd
suna66 <suna66@local.com>
2025-01-26 10:18:59 +0900

    add core functions for create/delete table

--- src/database.ts
+++ src/database.ts
@@ -2,6 +2,11 @@ import {
     DynamoDBClient,
     DescribeTableCommand,
     DescribeTableCommandOutput,
+    CreateTableCommand,
+    CreateTableCommandInput,
+    CreateTableCommandOutput,
+    DeleteTableCommand,
+    DeleteTableCommandOutput,
 } from "@aws-sdk/client-dynamodb";
 import {
     DynamoDBDocumentClient,
@@ -19,6 +24,40 @@ export type DynamoDBConfig = {
           }
         | undefined;
     region: string | undefined;
+};
+
+export enum AttributeType {
+    NUMBER = "N",
+    STRING = "S",
+    BINALY = "B",
+}
+
+export enum KeyType {
+    HASH = "HASH",
+    RANGE = "RANGE",
+}
+
+export enum IndexType {
+    LOCAL = "local",
+    GLOBAL = "global",
+}
+
+export type AttributeDefinition = {
+    attributeName: string;
+    attributeType: AttributeType;
+    keyType: KeyType | undefined;
+};
+
+export type SecondaryIndex = {
+    indexName: string;
+    indexType: IndexType;
+    attributeDefinitinList: Array<AttributeDefinition>;
+};
+
+export type CreateTableRequest = {
+    tableName: string;
+    attributeDefinitinList: Array<AttributeDefinition>;
+    indexes: Array<SecondaryIndex>;
 };
 
 export default class DynamoDBAccessor {
@@ -40,6 +79,112 @@ export default class DynamoDBAccessor {
 
     async describe(tableName: string): Promise<DescribeTableCommandOutput> {
         const command = new DescribeTableCommand({
+            TableName: tableName,
+        });
+
+        const response = await this.docClient.send(command);
+
+        return response;
+    }
+
+    async createTable(
+        req: CreateTableRequest
+    ): Promise<CreateTableCommandOutput> {
+        let createTableParam: CreateTableCommandInput = {
+            TableName: req.tableName,
+            BillingMode: "PAY_PER_REQUEST",
+            AttributeDefinitions: [],
+            KeySchema: [],
+        };
+
+        let attributeDefinitions = [];
+        let keySchema = [];
+        for (let attributes of req.attributeDefinitinList) {
+            const attr = {
+                AttributeName: attributes.attributeName,
+                AttributeType: attributes.attributeType,
+            };
+            attributeDefinitions.push(attr);
+            if (attributes.keyType != undefined) {
+                const schema = {
+                    AttributeName: attributes.attributeName,
+                    KeyType: attributes.keyType,
+                };
+                keySchema.push(schema);
+            }
+        }
+        createTableParam.AttributeDefinitions = attributeDefinitions;
+        createTableParam.KeySchema = keySchema;
+
+        let localSecondaryIndexes = undefined;
+        let globalSecondaryIndexs = undefined;
+        if (req.indexes != undefined) {
+            for (let index of req.indexes) {
+                if (index.indexType == IndexType.LOCAL) {
+                    let indexInfo = {
+                        IndexName: index.indexName,
+                        Projection: { ProjectionType: "ALL" },
+                    };
+                    let indexKeyScema = [];
+                    for (let schema of index.attributeDefinitinList) {
+                        if (
+                            schema.keyType == KeyType.HASH ||
+                            schema.keyType == KeyType.RANGE
+                        ) {
+                            indexKeyScema.push({
+                                AttributName: schema.attributeName,
+                                KeyType: schema.keyType,
+                            });
+                        }
+                    }
+                    indexInfo["KeySchema"] = indexKeyScema;
+                    if (localSecondaryIndexes == undefined)
+                        localSecondaryIndexes = [];
+                    localSecondaryIndexes.push(indexInfo);
+                } else {
+                    let indexInfo = {
+                        IndexName: index.indexName,
+                        Projection: { ProjectionType: "ALL" },
+                        ProvisionedThroughput: {
+                            ReadCapacityUnits: 1,
+                            WriteCapacityUnits: 1,
+                        },
+                    };
+                    let indexKeyScema = [];
+                    for (let schema of index.attributeDefinitinList) {
+                        if (
+                            schema.keyType == KeyType.HASH ||
+                            schema.keyType == KeyType.RANGE
+                        ) {
+                            indexKeyScema.push({
+                                AttributName: schema.attributeName,
+                                KeyType: schema.keyType,
+                            });
+                        }
+                    }
+                    indexInfo["KeySchema"] = indexKeyScema;
+                    if (globalSecondaryIndexs == undefined)
+                        globalSecondaryIndexs = [];
+                    globalSecondaryIndexs.push(indexInfo);
+                }
+            }
+        }
+        if (localSecondaryIndexes != undefined) {
+            createTableParam["LocalSecondaryIndexes"] = localSecondaryIndexes;
+        }
+        if (globalSecondaryIndexs != undefined) {
+            createTableParam["GlobalSecondaryIndexes"] = globalSecondaryIndexs;
+        }
+
+        const command = new CreateTableCommand(createTableParam);
+
+        const response = await this.docClient.send(command);
+
+        return response;
+    }
+
+    async deleteTable(tableName: string): Promise<DeleteTableCommandOutput> {
+        const command = new DeleteTableCommand({
             TableName: tableName,
         });
 
